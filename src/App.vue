344c1a06c058b8aa2ed5ccdb8965f5f62ef37b92
so0126 <nada981226@gmail.com>
2025-10-01 14:50:56 +0900

    chore: MyPageSideBar 이름을 MyWorldSideBar로 바꿈

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import AppHeader from './components/AppHeader.vue';
-import AppMenu from './components/Minihome/MyPageSideBar.vue';
+import AppMenu from './components/MyWorldSideBar.vue';
 </script>
 
 <style>
